77e0b21ebc20ce8105156f8bcdb78f5f8c5ee669
rakin061 <salmankaderrakin@gmail.com>
2023-08-18 20:13:54 +0600

    script for smaller dataset..

---
+++ script.py
@@ -0,0 +1,21 @@
+import json
+with (
+  open("hotpot_train_v1.1.json","r") as file_data1, open("hotpot_dev_distractor_v1.json","r") as file_data2, open("hotpot_dev_fullwiki_v1.json","r") as file_data3  ):
+  data1=json.load(file_data1)
+  data2=json.load(file_data2)
+  data3=json.load(file_data3)
+
+json_data1=json_data2=json_data3 =[]
+for i in range(10):
+    json_data1.append(data1[i])
+    json_data2.append(data2[i])
+    json_data3.append(data3[i])
+
+json_object1 = json.dumps(json_data1, indent=4)
+json_object2 = json.dumps(json_data2, indent=4)
+json_object3 = json.dumps(json_data3, indent=4)
+
+with open("sample_train.json", "w") as outfile1, open("sample_dev_distractor.json", "w") as outfile2,open("sample_dev_fullwiki.json", "w") as outfile3 :
+    outfile1.write(json_object1)
+    outfile2.write(json_object2)
+    outfile3.write(json_object3)
