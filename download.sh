482e352bfaa96fde169e90d3da12b89eb1374c2d
rakin061 <salmankaderrakin@gmail.com>
2023-08-18 11:16:15 +0600

    Check whether file exists before donwloading..

--- download.sh
+++ download.sh
@@ -1,14 +1,14 @@
 
 # Download Hotpot Data
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
-wget http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
+wget -N http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json
+wget -N http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
+wget -N http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
 
 # Download GloVe
 GLOVE_DIR=./
 mkdir -p $GLOVE_DIR
-wget http://nlp.stanford.edu/data/glove.840B.300d.zip -O $GLOVE_DIR/glove.840B.300d.zip
-unzip $GLOVE_DIR/glove.840B.300d.zip -d $GLOVE_DIR
+wget -N http://nlp.stanford.edu/data/glove.840B.300d.zip 
+ls $GLOVE_DIR/glove.840B.300d.txt || unzip $GLOVE_DIR/glove.840B.300d.zip -d $GLOVE_DIR
 
 # Download Spacy language models
 python3 -m spacy download en
